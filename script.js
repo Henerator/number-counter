8ee86f0c333517ebbd00545f0f8eafffa6ff2b5e
WhiteBlack <whiteblacks@yandex.ru>
2020-12-13 23:20:02 +0400

    add multiple counters

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
-let counterValue = 0;
-const controlA = 0.1;
-const controlB = 0.1;
-const duration = 1;
-const delay = 2;
-const counterElement = document.getElementById('counter');
+const configs =  [
+    {
+        value : 0,
+        min: 0,
+        max: 100,
+        controlA : 0.9,
+        controlB : 0.9,
+        duration : 4,
+        delay : 2,
+    },
+    {
+        value : 0,
+        min: 1000,
+        max: 9999,
+        controlA : 0.1,
+        controlB : 0.1,
+        duration : 5,
+        delay : 2,
+    },
+    {
+        value : 0,
+        min: 0,
+        max: 100000,
+        controlA : 0.1,
+        controlB : 0.1,
+        duration : 1,
+        delay : 2,
+    },
+];
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function update() {
-    const nextValue = getRandomInt(0, 100000);
-    const numberCounter = new NumberCounter(counterValue, nextValue, controlA, controlB, duration);
+function createElements(configs) {
+    configs.forEach((config, index) => {
+        const element = document.createElement('div');
+        element.classList.add('counter');
+
+        config.element = element;
+        document.body.appendChild(element);
+    });
+}
+
+function updateCounter(config) {
+    const nextValue = getRandomInt(config.min, config.max);
+    const numberCounter = new NumberCounter(
+        config.value,
+        nextValue,
+        config.controlA,
+        config.controlB,
+        config.duration
+    );
     numberCounter.onChange = (value) => {
-        counterElement.innerText = Math.floor(value);
+        config.element.innerText = Math.floor(value);
     };
     numberCounter.onTransitionEnd = () => {
-        setTimeout(update, delay * 1000);
+        setTimeout(() => updateCounter(config), config.delay * 1000);
     };
     numberCounter.startTransition();
 
-    counterValue = nextValue;
+    config.value = nextValue;
 }
 
-update();
+function update(configs) {
+    configs.forEach(config => updateCounter(config));
+}
+
+createElements(configs);
+update(configs);
